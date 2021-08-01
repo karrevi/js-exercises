695d6d6a5ec70b8f67d8b80d9be453d4dbbaae8f
Carlos Revilla <clro26.k@gmail.com>
2021-08-01 13:27:59 +0200

    functions with arguments and returns

--- functions.js
+++ functions.js
@@ -1,7 +1,18 @@
-function suma_y_muestra() {
-    const numero2 = 5
-    const numero1 = 3
-    resultado = numero1 + numero2;
-    alert("El resultado es " + resultado);
+// function suma_y_muestra() {
+//     const numero2 = 5
+//     const numero1 = 3
+//     resultado = numero1 + numero2;
+//     alert("El resultado es " + resultado);
+// }
+// suma_y_muestra();
+
+// ARGUMENTOS Y VALORES DE RETORNO
+
+function suma_y_muestra(primerNumero, segundoNumero) {
+    var resultado = primerNumero + segundoNumero;
+    alert("el resultado es " + resultado)
 }
-suma_y_muestra();
+var numero1 = 3;
+var numero2 = 5;
+
+suma_y_muestra(numero1, numero2);
