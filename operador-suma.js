f1c664228ec449faacf01e2646bc70f82e3b6d45
Carlos Revilla <clro26.k@gmail.com>
2021-08-01 13:09:26 +0200

    firts commit

---
+++ operador-suma.js
@@ -0,0 +1,4 @@
+const num1 = 3;
+const num2 = 5;
+
+document.write(num1 + num2)
---
+++ uso-comillas.js
@@ -0,0 +1,5 @@
+// const mensaje = "Hola mundo! \n Qué facil es incluir \'comillas simples\' \n y \"comillas dobles\"";
+// alert(mensaje);
+
+const mensaje = "Hello World!"
+document.write(mensaje)
